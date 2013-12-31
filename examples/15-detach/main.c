82a1a3d0720229cdfef7387fc6e5945159c35d31
Thorsten Lorenz <thlorenz@gmx.de>
2013-12-30 21:43:51 -0500

    detach

--- examples/15-detach/main.c
+++ examples/15-detach/main.c
@@ -24,19 +24,30 @@ int main() {
   int r;
   loop = uv_default_loop();
 
-  const char* exepath = "mkdir";
-  char *args[3] = { (char*) exepath, "test-dir", NULL };
+  const char* exepath = "sleep";
+  char *args[3] = { (char*) exepath, "100", NULL };
 
   // https://github.com/thlorenz/libuv-dox/blob/master/types.md#uv_process_options_t
   options.exit_cb = on_exit;
   options.file = exepath;
   options.args = args;
+  options.flags = UV_PROCESS_DETACHED;
 
 
-  fprintf(stderr, "spawning: %s %s\n", exepath, args[1]);
+  fprintf(stderr, "spawning detached: %s %s\n", exepath, args[1]);
   r = uv_spawn(loop, &child_req, &options);
   if (r) ERROR("spawning child process", r);
 
+  fprintf(stderr, "Launched sleep with PID %d\n", child_req.pid);
+  fprintf(stderr, "See it runnig via: ps aux | grep %d", child_req.pid);
+
+  assert(uv_has_ref((uv_handle_t*) &child_req));
+
+  // manually decrease event loop's ref count to allow our process to exit, making this a fire and forget
+  // https://github.com/thlorenz/libuv-dox/blob/master/methods.md#reference-count
+  uv_unref((uv_handle_t*) &child_req);
+
+  assert(!uv_has_ref((uv_handle_t*) &child_req));
 
   return uv_run(loop, UV_RUN_DEFAULT);
 }
